70fd02ed5d0e605863ba4a1ba78d3f79f678ded3
guoshuangfei <182175751@qq.com>
2017-11-19 21:42:44 +0800

    sea food project created

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -7,19 +7,27 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func CameraButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+        
+        
     }
-
-
+    
 }
 
