655495fefed58e9721cd85de93d0bcca3c12a408
guoshuangfei <182175751@qq.com>
2017-11-20 10:25:48 +0800

    first time to commit the code

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -10,20 +10,44 @@ import UIKit
 import CoreML
 import Vision
 
+//TODO 1: add the protocol and delegate
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var ImageView: UIImageView!
+    //TODO 3: creat an object of UIImagePickerController
+    let imagePicker = UIImagePickerController()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+    //TODO 4: set the property of the object
         
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
         
         
     }
-
-    @IBAction func CameraButtonPressed(_ sender: UIBarButtonItem) {
+    
+    //TODO 5:
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+        
+        ImageView.image = userPickedImage
+        
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    //TODO 2: link the camera button and the image view outlet
+    @IBAction func CameraButtonPressed(_ sender: UIBarButtonItem) {
+     
+        present(imagePicker, animated: true, completion: nil)
         
         
         
